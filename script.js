ee015c1b58da763105d3c27638d460ece362cb42
mahmudaakter63 <mahmudaakter2632@gmail.com>
2024-09-13 15:04:23 +0600

    first commit

---
+++ script.js
@@ -0,0 +1,29 @@
+function myFunction(){
+    document.getElementById("name").innerHTML = "United States";
+}
+
+function change(){
+    document.getElementById('demo').innerHTML = "Kamala Harris";
+}
+
+function myimage() {
+    document.getElementById('myimage').src='c:\Users\WALTON\Downloads\Rectangle 29 (1).png'
+   }
+
+ function image(){
+     document.getElementById('myimage').src='c:\Users\WALTON\Downloads\Rectangle 20.png'
+ }
+
+ 
+function fontChange(){
+    document.getElementById('font').style.fontSize="35px";
+}
+
+function wife(){
+    document.getElementById('wife-none').style.display="none"
+}
+
+
+function showtext(){
+    document.getElementById('show').style.display='block'
+}
